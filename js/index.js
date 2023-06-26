4278836239bfa1e287c55bf6e76e731c49608ed1
o.kononenko <enko@solvve.com>
2023-06-26 20:09:03 +0300

    qwd

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const title = `Test title with spaces \n\n`;
+const title = `Test title with spaces <br><br>`;
 
 var metaTag = document.querySelector('meta[property="og:title"]');
 var titleWithLineBreak = title.replace(/\n\n/g, '&nbsp;');
